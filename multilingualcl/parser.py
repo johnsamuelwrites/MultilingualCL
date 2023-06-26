f0b3fe25506c4a1691dff38a8e538965823c3261
John Samuel <johnsamuelwrites@gmail.com>
2023-06-26 13:28:39 +0200

    Support multilingual commands

--- multilingualcl/parser.py
+++ multilingualcl/parser.py
@@ -6,51 +6,70 @@ def parse_user_command(command_map, current_locale, user_input):
     # Parse the user command using argparse and the command map
     parser = argparse.ArgumentParser()
     user_input_split = user_input.split()
+    options_map = dict()
 
     # Find the command with matching name in the current locale
     matching_command = None
     for command_name, command_details in command_map.items():
-        if command_details.get('locales', {}).get(current_locale):
-            if user_input_split[0] in command_details.get('locales', {}).get(current_locale).get("names", []):
-               matching_command = command_details.get('locales', {}).get(current_locale)
-               break
+        if command_details.get("locales", {}).get(current_locale):
+            if user_input_split[0] in command_details.get("locales", {}).get(
+                current_locale
+            ).get("names", []):
+                matching_command = command_details.get("locales", {}).get(
+                    current_locale
+                )
+                break
 
     if matching_command:
         # Create a parser for the matching command
-        subparsers = parser.add_subparsers(dest='command')
+        subparsers = parser.add_subparsers(dest="command")
         command_parser = subparsers.add_parser(command_name)
+        for translated_name in matching_command["names"]:
+            if translated_name == user_input_split[0]:
+                user_input = user_input.replace(
+                    user_input_split[0], command_details["linux_command"], 1
+                )  # Only the first occurence
+                break
 
         # Add options for the command
-        for option in matching_command.get('options', []):
-            option_name = option['name']
-            option_aliases = option.get('aliases', [])
-            command_parser.add_argument(option_name, *option_aliases, action='store_true')
+        for option in matching_command.get("options", []):
+            option_name = option["name"]
+            option_aliases = option.get("aliases", [])
+
+            options_map[option_name.replace("-", "")] = option_name
+            for alias in option_aliases:
+                options_map[alias.replace("-", "")] = option_name
+            command_parser.add_argument(
+                option_name, *option_aliases, action="store_true"
+            )
 
         # Add subcommands for the command
-        subcommand_parsers = command_parser.add_subparsers(dest='subcommand')
-        for subcommand in matching_command.get('subcommands', []):
-            subcommand_parser = subcommand_parsers.add_parser(subcommand['name'])
+        subcommand_parsers = command_parser.add_subparsers(dest="subcommand")
+        for subcommand in matching_command.get("subcommands", []):
+            subcommand_parser = subcommand_parsers.add_parser(subcommand["name"])
 
             # Add options for the subcommand
-            for option in subcommand.get('options', []):
-                option_name = option['name']
-                option_aliases = option.get('aliases', [])
-                subcommand_parser.add_argument(option_name, *option_aliases, action='store_true')
+            for option in subcommand.get("options", []):
+                option_name = option["name"]
+                option_aliases = option.get("aliases", [])
+
+                options_map[option_name.replace("-", "")] = option_name
+                for alias in option_aliases:
+                    options_map[alias.replace("-", "")] = option_name
+                subcommand_parser.add_argument(
+                    option_name, *option_aliases, action="store_true"
+                )
 
         # Parse the user command with optional positional argument
         args_list = user_input.split()
         parsed_args, remaining = parser.parse_known_args(args_list)
-    
+
         # Parse the positional argument separately if available
-        if matching_command.get('positionals', []):
-            #positionals = matching_command['positionals']
-            if remaining: 
-                parsed_args.positional = remaining 
-                option_name = option['name']
+        if matching_command.get("positionals", []):
+            # positionals = matching_command['positionals']
+            if remaining:
+                parsed_args.positional = remaining
+                option_name = option["name"]
 
         # Parse the user command
-        return parsed_args
-
-
-
-
+        return parsed_args, options_map
--- multilingualcl/repl.py
+++ multilingualcl/repl.py
@@ -2,10 +2,12 @@ import readline
 import difflib
 from termcolor import colored
 import locale
+import subprocess
 from command_map import load_command_map
-from parser import parse_user_command
+from command import obtain_command_from_translation
 
 HISTORY_FILE = ".repl_history"  # File to store command history
+
 
 def load_history():
     try:
@@ -13,28 +15,34 @@ def load_history():
     except FileNotFoundError:
         pass
 
+
 def save_history():
     readline.write_history_file(HISTORY_FILE)
+
 
 def initialize():
     load_history()
     readline.parse_and_bind("tab: complete")  # Enable tab completion
     readline.set_auto_history(True)  # Enable command history
 
+
 def get_suggestions(text, options):
-    matches = difflib.get_close_matches(text, options, n=5, cutoff=0.5)  # Limit suggestions to 5 and set cutoff threshold
+    matches = difflib.get_close_matches(
+        text, options, n=5, cutoff=0.5
+    )  # Limit suggestions to 5 and set cutoff threshold
     return matches
+
 
 def highlight_match(text, match):
     highlighted_text = colored(match, "yellow")
     return text.replace(match, highlighted_text)
 
-def evaluate_command(command, command_map, current_locale):
-    print(command)
-    parsed_command = parse_user_command(command_map, current_locale, command)
-    print(parsed_command)
 
-    return "Result: " + command
+def evaluate_command(command, command_map, current_locale):
+    real_command = obtain_command_from_translation(command_map, current_locale, command)
+    subprocess.run(real_command.split())
+    return "Result: " + real_command
+
 
 def repl():
     while True:
@@ -48,17 +56,15 @@ def repl():
 
             save_history()
 
-
             # Evaluation
             result = evaluate_command(command, command_map, current_locale)
-            print(result)
             print(colored(result, "green"))  # Print result in green color
 
         except (KeyboardInterrupt, EOFError):
             print(colored("\nExiting REPL...", "red"))
             break
 
+
 if __name__ == "__main__":
     initialize()
     repl()
-
