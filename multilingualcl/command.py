eebddf9ce3734c934aa9bc0f6c46facdf1bedc3e
John Samuel <johnsamuelwrites@gmail.com>
2023-06-26 13:29:04 +0200

    Support multilingual commands

--- multilingualcl/command.py
+++ multilingualcl/command.py
@@ -1,132 +1,24 @@
-# alias in different languages 
-alias={}
-alias["en_US"] = "alias"
-
-# option in different languages 
-option={}
-option["en_US"] = "option"
-
-# argument in different languages 
-argument={}
-argument["en_US"] = "argument"
-
-# positional argument in different languages 
-positionalargument={}
-positionalargument["en_US"] = "positional argument"
-
-# short form in different languages 
-shortform={}
-shortform["en_US"] = "short form"
-
-# long form in different languages 
-longform={}
-longform["en_US"] = "long form"
-
-# documentation in different languages 
-documentation={}
-documentation["en_US"] = "documentation"
-
-# resources and their labels in different languages
-user_label = {}
-user_label["en_US"] = ["user"]
-
-group_label = {}
-group_label["en_US"] = ["group"]
-
-process_label = {}
-process_label["en_US"] = ["process"]
-
-memory_label = {}
-memory_label["en_US"] = ["memory"]
-
-device_label = {}
-device_label["en_US"] = ["device"]
-
-connection_label = {}
-connection_label["en_US"] = ["connection"]
-
-file_label = {}
-file_label["en_US"] = ["file"]
-
-directory_label = {}
-directory_label["en_US"] = ["directory"]
-
-date_label = {}
-date_label["en_US"] = ["date"]
+from parser import parse_user_command
 
 
-command = {}
-command["en_US"] = {}
-command["en_US"]["create"] = {}
-command["en_US"]["create"][alias["en_US"]] = ["make"]
-command["en_US"]["create"][option["en_US"]] = [
-        user_label["en_US"][0],
-        group_label["en_US"][0],
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
+def obtain_command_from_translation(command_map, current_locale, user_input):
+    parsed_args, options_map = parse_user_command(
+        command_map, current_locale, user_input
+    )
+    unparsed_args = []  # Add the command name
+    for arg in vars(parsed_args):
+        value = getattr(parsed_args, arg)
+        if value is not None:
+            if isinstance(value, bool):  # Handle boolean flags without values
+                if value and arg in options_map:
+                    option = options_map[arg]
+                    unparsed_args.append(f"{option}")
+            else:
+                if type(value) == list:
+                    unparsed_args.append(" ".join(value))
+                else:
+                    unparsed_args.append(str(value))
 
-command["en_US"]["read"] = {}
-command["en_US"]["read"][alias["en_US"]] = ["show", "print"]
-command["en_US"]["read"][option["en_US"]] = [
-        user_label["en_US"][0],
-        date_label["en_US"][0],
-        group_label["en_US"][0],
-        process_label["en_US"][0],
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
+    command_string = " ".join(unparsed_args)
 
-command["en_US"]["update"] = {}
-command["en_US"]["update"][alias["en_US"]] = []
-command["en_US"]["update"][option["en_US"]] = [
-        user_label["en_US"][0],
-        group_label["en_US"][0],
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["delete"] = {}
-command["en_US"]["delete"][alias["en_US"]] = []
-command["en_US"]["delete"][option["en_US"]] = [
-        user_label["en_US"][0],
-        group_label["en_US"][0],
-        process_label["en_US"][0],
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["list"] = {}
-command["en_US"]["list"][alias["en_US"]] = []
-command["en_US"]["list"][option["en_US"]] = [
-        user_label["en_US"][0],
-        memory_label["en_US"][0],
-        device_label["en_US"][0],
-        group_label["en_US"][0],
-        process_label["en_US"][0],
-        connection_label["en_US"][0],
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["compress"] = {}
-command["en_US"]["compress"][alias["en_US"]] = []
-command["en_US"]["compress"][option["en_US"]] = [
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["decompress"] = {}
-command["en_US"]["decompress"][alias["en_US"]] = []
-command["en_US"]["decompress"][option["en_US"]] = [
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["link"] = {}
-command["en_US"]["link"][option["en_US"]] = [
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["copy"] = {}
-command["en_US"]["copy"][option["en_US"]] = [
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
-
-command["en_US"]["rename"] = {}
-command["en_US"]["rename"][option["en_US"]] = [
-        file_label["en_US"][0],
-        directory_label["en_US"][0]]
+    return command_string
